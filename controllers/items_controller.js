6d5c848606e32cfda48358996d8d784561e8b304
Tomd304 <tomd304@live.co.uk>
2022-02-09 22:44:21 +0000

    Individual item pages setup

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -1,8 +1,15 @@
 var Items = require("../models/item");
 
+// MAIN PAGE / - SHOWS LIST OF ALL ITEMS
 exports.index = (req, res, next) => {
   Items.find().exec((err, items) => {
-    console.table(items);
     res.render("inventory", { title: "Inventory", items: items });
+  });
+};
+
+// ITEM DETAIL PAGE /item/:id - SHOWS ITEM DETAIL
+exports.item_detail = (req, res, next) => {
+  Items.findById(req.params.id).exec((err, item) => {
+    res.render("item_detail", { title: item.name, item: item });
   });
 };
--- routes/inventory.js
+++ routes/inventory.js
@@ -5,4 +5,6 @@ var items_controller = require("../controllers/items_controller");
 /* GET home page. */
 router.get("/", items_controller.index);
 
+router.get("/item/:id", items_controller.item_detail);
+
 module.exports = router;
