a070e63a96b6863a9980293ff415eebfcc6279e7
Tomd304 <tomd304@live.co.uk>
2022-02-09 22:32:08 +0000

    MONGODB_URI added to hidden js folder

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = process.env.MONGODB_URI;
+var { MONGODB_URI } = require("./env");
+var mongoDB = MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
